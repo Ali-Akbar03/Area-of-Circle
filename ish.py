b20196ab85c7c0a5bb07907eeac97574ac602874
Ali Akbar Khaled <aliishmam23@gmail.com>
2025-02-20 03:52:32 +0600

    Add files via upload

---
+++ ish.py
@@ -0,0 +1,9 @@
+# A basic python code to determie the area of circle
+
+radius = (int)(input("Enter the raius of the circle - ")) # taking user input 
+pi=  3.1416
+
+area = pi * (radius**2) # number of star indicates the power  
+
+print("The area of the circle is = ", area) # final answer 
+
